e104ad6c5e7b5900c6d4a6abfd30256c976f49fc
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-15 08:52:47 +0100

    refac: add update rewards function

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -13,9 +13,24 @@ contract Staking {
 
     //total supply
     uint256 public s_totalSupply;
+    uint256 public constant REWARD_RATE = 100;
+    uint256 public s_rewardPerTokenStored;
+    uint256 public s_lastUpdateTime;
+
+    modifier updateReward(address account){
+        s_rewardPerTokenStored  = rewardPerToken();
+        s_lastUpdateTime = block.timestamp;
+    }
 
     constructor (address stakingToken) {
         s_stakingToken = IERC20(stakingToken);  
+    }
+
+    function rewardPerToken() public view returns(uint256){
+        if(s_totalSupply == 0){
+            return s_rewardPerTokenStored;
+        }
+        return s_rewardPerTokenStored +(((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18)/ s_totalSupply );
     }
 
     function stake(uint256 _amount) external {
@@ -25,6 +40,16 @@ contract Staking {
 
         //transfers the token to the contract
         bool success = s_stakingToken.transferFrom(msg.sender, address(this), _amount);
+        if(!success){
+            revert Staking__TransferFailed();
+        }
+    }
+
+    function withdraw(uint256 _amount) external {
+        s_balances[msg.sender] -= _amount;
+        s_totalSupply -= _amount;
+
+        bool success = s_stakingToken.transfer(msg.sender, _amount);
         if(!success){
             revert Staking__TransferFailed();
         }
