174f45b0ed18a436fcdd2a572e3b32d5b10e3aa5
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-15 10:55:03 +0100

    feat: add tests

---
+++ test/staking.test.js
@@ -0,0 +1,19 @@
+const { ethers, deployments } = require("hardhat");
+
+describe('Staking Test', async() => {
+    let staking, rewardToken, deployer, stakeAmount;
+
+    beforeEach(async() => {
+        const accounts = await ethers.getSigners();
+        const deployer = accounts[0];
+        await deployments.fixture(["all"]);
+
+        staking = await ethers.getContract("Staking");
+        rewardToken = await ethers.getContract("RewardToken");
+
+        stakeAmount = ethers.utils.parseEther("100000")
+
+    })
+
+    
+});
