37ced198bd57dfb4acbe66b80bd7a7390fbab051
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-15 11:39:08 +0100

    refac: add test for user staking

--- test/staking.test.js
+++ test/staking.test.js
@@ -1,4 +1,8 @@
 const { ethers, deployments } = require("hardhat");
+const{ moveBlocks } = require("../utils/move-blocks");
+const{ moveTime } = require("../utils/move-time");
+
+const SECONDS_IN_A_DAY = 86400;
 
 describe('Staking Test', async() => {
     let staking, rewardToken, deployer, stakeAmount;
@@ -12,6 +16,19 @@ describe('Staking Test', async() => {
         rewardToken = await ethers.getContract("RewardToken");
 
         stakeAmount = ethers.utils.parseEther("100000")
+
+    })
+
+    it("Allows Users to stake and claim rewards", async() => {
+        await rewardToken.approve(staking.address, stakeAmount);
+        await staking.stake(stakeAmount);
+        const startingEarned = await staking.earned(deployer.address);
+        console.log(`Starting Earned__-- ${startingEarned}`);
+
+        await moveTime(SECONDS_IN_A_DAY);
+        await moveBlocks(1);
+        const endingEarned = await staking.earned(deployer.address);
+        console.log(`ending Earned__-- ${endingEarned}`);
 
     })
 
